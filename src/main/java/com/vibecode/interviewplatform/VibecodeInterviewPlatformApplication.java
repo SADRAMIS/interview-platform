a641ef7b245310f6ed9ff2d186edafc171ac322c
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-12-05 17:13:06 +0300

    Init commit

---
+++ src/main/java/com/vibecode/interviewplatform/VibecodeInterviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vibecode.interviewplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibecodeInterviewPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibecodeInterviewPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vibecode/interviewplatform/VibecodeInterviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibecode.interviewplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibecodeInterviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
